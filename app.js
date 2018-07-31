d485f7ed900e1c6db100a4c56b7b793233750af4
John Flash <johngood4real@yahoo.co.in>
2018-07-31 13:33:58 +0100

    implemented jwt auth

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ var BmsApp = angular.module('BmsApp',
 )
 	.constant('API_HOST', BMS_API) // from config.js
 	.constant('API_HOST_AUTH', AUTH_API) // config.js
+	.config(function Config($httpProvider, jwtOptionsProvider) {
+
+		jwtOptionsProvider.config({
+			unauthenticatedRedirectPath: 'login',
+			whiteListedDomains:['curacel.co','localhost'],
+			tokenGetter: ['UserService',function(UserService) {
+				return UserService.loadToken();
+			}],
+			unauthenticatedRedirector: ['$state', function($state) {
+				$state.go(this.unauthenticatedRedirectPath);
+			}]
+		});
+
+		$httpProvider.interceptors.push('jwtInterceptor');
+	})
