ad07138a9f63a3e0dd3c5c10db858c067ea07bb9
John Flash <johngood4real@yahoo.co.in>
2018-08-06 17:40:50 +0100

    admin lte

--- modules/global/app/js/controllers.js
+++ modules/global/app/js/controllers.js
@@ -30,7 +30,7 @@ BmsApp.controller('LoginCtrl', function($scope,$activityIndicator,UserService,$s
 BmsApp.controller('AppCtrl', function($scope,$activityIndicator,UserService,$state) {
     $scope.user = UserService.getUserData();
 
-
+    $('body').layout('fix')
 
     $scope.logout = function(){
         swal({
