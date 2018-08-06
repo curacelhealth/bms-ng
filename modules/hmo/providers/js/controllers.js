64d93e09b5758c168b96feb11eb378567823890b
John Flash <johngood4real@yahoo.co.in>
2018-08-06 09:41:32 +0100

    fixed providers list datatables to use api

--- modules/hmo/providers/js/controllers.js
+++ modules/hmo/providers/js/controllers.js
@@ -2,11 +2,38 @@
  * Created by JFlash on 7/31/18.
  */
 //providers List controller
-BmsApp.controller('ProvidersListCtrl', function($scope, $activityIndicator, UserService, $state, DTOptionsBuilder, DTColumnBuilder) {
+BmsApp.controller('ProvidersListCtrl', function($scope,$compile, $activityIndicator, UserService, $state, DTOptionsBuilder, DTColumnBuilder,ProviderService) {
     var vm = this;
-    vm.dtOptions = DTOptionsBuilder.fromSource('modules/hmo/providers/data.json')
-        .withPaginationType('full_numbers');
+    //vm.dtOptions = DTOptionsBuilder.fromSource('modules/hmo/providers/data.json') 
+    // .withPaginationType('full_numbers');
 
+    vm.dtInstance = {}; //instance ref for data tables
+    vm.filters = {}; // filters
+
+
+    //init options for datatable grid on this scope, using ajax for data source
+    vm.dtOptions = DTOptionsBuilder.newOptions()
+        .withOption('ajax', {
+            // Either you specify the AjaxDataProp here
+            // dataSrc: 'data',
+            url: ProviderService.fetchListDTUrl(), // get url from service for datatable requests
+            type: 'GET',
+            data: vm.filters,
+            headers: {
+                Authorization: 'Bearer ' + UserService.loadToken() // add token for authentication
+            }
+        })
+        // or here
+        .withDataProp('data')
+        .withOption('processing', true)
+        .withOption('serverSide', true)
+        .withPaginationType('full_numbers')
+
+        .withOption('fnRowCallback',
+            function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+                $compile(nRow)($scope); // this ensures angular directives are compiled after each row is created
+            });
+    
     vm.dtColumns = [
         DTColumnBuilder.newColumn('id').withTitle('ID'),
         DTColumnBuilder.newColumn('name').withTitle('Name'),
@@ -18,9 +45,10 @@ BmsApp.controller('ProvidersListCtrl', function($scope, $activityIndicator, User
         DTColumnBuilder.newColumn('provider_tier_id').withTitle('Provider Tier ID'),
         DTColumnBuilder.newColumn(null).withTitle('Actions').notSortable()
             .renderWith(function(data, type, full, meta) {
-                return '<a href="#/hmo/providers/view/'+data.id+'" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon m-btn--pill" title="Edit details"> <i class="la la-book"></i>View</a><br>'+
-                	   '<a href="#/hmo/providers/view/'+data.id+'" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon m-btn--pill" title="Edit details"> <i class="la la-edit"></i>Edit</a><br>'+
-                	   '<a href="#/hmo/providers" class="m-portlet__nav-link btn m-btn m-btn--hover-danger m-btn--icon m-btn--icon m-btn--pill" title="Delete"> <i class="la la-trash"></i> Delete</a>';
+                // you should use full.id instead of data.id here
+                // they shouldnt be able to delete a provider here, so i removed the delete button
+                return '<a ui-sref="hmo.providersCreate({id:'+full.id+'})" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon m-btn--pill" title="Edit details"> <i class="la la-book"></i>View</a><br>'+
+                	   '<a ui-sref="hmo.providersView({id:'+full.id+'})" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon m-btn--pill" title="Edit details"> <i class="la la-edit"></i>Edit</a><br>'
             }),
     ];
     
--- modules/hmo/providers/js/service.js
+++ modules/hmo/providers/js/service.js
@@ -6,11 +6,16 @@ BmsApp
 
         return {
             fetchList: function () {
-                
+                return $http.get(API_HOST+'/providers/')
+            },
+
+            //return api url for handling datatable requests ..i.e DT
+            fetchListDTUrl:function () {
+                return   API_HOST+'/providers/DT' 
             },
             
             fetchSingleByID: function (id) {
-                
+                return $http.get(API_HOST+'/providers/'+id)
             }
 
 
