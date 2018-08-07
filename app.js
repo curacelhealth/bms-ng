7e81f4a05fec1bcaf2c3828186d64399d8ec357e
John Flash <johngood4real@yahoo.co.in>
2018-08-07 10:56:20 +0100

    auto logout on refresh

--- app.js
+++ app.js
@@ -25,3 +25,8 @@ var BmsApp = angular.module('BmsApp',
 
 		$httpProvider.interceptors.push('jwtInterceptor');
 	})
+	.run(["$rootScope", "$state","authManager", function($rootScope, $state,authManager) {
+		$rootScope.$state = $state; // state to be accessed from view
+		authManager.checkAuthOnRefresh();
+		authManager.redirectWhenUnauthenticated();
+	}]);
