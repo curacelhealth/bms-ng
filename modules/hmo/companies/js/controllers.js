b89acffd149ac7080ef2eb7c9fbb77bf2d78335b
Femi <cocofemi@gmail.com>
2018-08-07 23:08:32 +0100

    wired up companiesListCtrl API

--- modules/hmo/companies/js/controllers.js
+++ modules/hmo/companies/js/controllers.js
@@ -2,8 +2,53 @@
  * Created by JFlash on 7/31/18.
  */
 //Companies List controller
-BmsApp.controller('HmoCompaniesListCtrl', function($scope,$activityIndicator,UserService,$state) {
-    
+BmsApp.controller('HmoCompaniesListCtrl', function($scope,$activityIndicator,CompaniesService,$state,DTColumnBuilder,DTOptionsBuilder,UserService) {
+   	$scope.dtInstance = {}; //instance reference for datatables
+    $scope.filters = {}; // filters
+
+    //initialize options for datatable grid on this scope
+    $scope.dtOptions = DTOptionsBuilder.newOptions()
+        .withOption('ajax', {
+            // Either you specify the AjaxDataProp here
+            // dataSrc: 'data',
+            url: CompaniesService.fetchListDTUrl(),
+            type: 'GET',
+            data: $scope.filters,
+            headers: {
+                Authorization: 'Bearer ' + UserService.loadToken()
+            }
+        })
+        // or here
+        .withDataProp('data')
+        .withOption('processing', true)
+        .withOption('serverSide', true)
+        .withPaginationType('full_numbers')
+
+        .withOption('fnRowCallback',
+            function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+                $compile(nRow)($scope);
+            });
+
+            //create columns for this grid
+    $scope.dtColumns = [
+        DTColumnBuilder.newColumn('company_name').withTitle('Company Name').notSortable(),
+        DTColumnBuilder.newColumn('company_plan').withTitle('Plan')
+            .renderWith(function (data,type,full) {
+               return data +' '+full.plan
+            }),
+        
+        DTColumnBuilder.newColumn('number_of_lives').withTitle('No Of Lives'),
+        DTColumnBuilder.newColumn('sex').withTitle('Sex'),
+        DTColumnBuilder.newColumn('company_rep_name').withTitle('Company Rep.Name'),
+        DTColumnBuilder.newColumn('rep_telephone').withTitle('Rep.Telephone'),
+        DTColumnBuilder.newColumn('rep_email').withTitle('Rep.Email'),
+        DTColumnBuilder.newColumn('address').withTitle('Address'),
+        DTColumnBuilder.newColumn('state').withTitle('State'),
+        DTColumnBuilder.newColumn('action').withTitle('Action').notSortable()
+                       .renderWith(function(data, type, full, meta) {
+                return '<a ui-sref="hmo.companiesCreate({id:'+full.id+'})" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon m-btn--pill" title="Edit details"> <i class="la la-book"></i>View</a><br>'
+            }),
+    ];
 });
 
 //provider create / edit controller
--- modules/hmo/companies/js/service.js
+++ modules/hmo/companies/js/service.js
@@ -2,16 +2,21 @@
  * Created by JFlash on 12/14/17.
  */
 BmsApp
-    .service('CompanyService', function ( $http,API_HOST,$httpParamSerializer) {
+    .service('CompaniesService', function ( $http,API_HOST) {
 
         return {
             fetchList: function (search,limit) {
-                var qs = $httpParamSerializer({search:search||'', limit:limit||5})
+                 var qs = $httpParamSerializer({search:search||'', limit:limit||5})
                 return $http.get(API_HOST+'/companies/?'+qs);
             },
-            
+
+ 		//return api url for handling datatable requests ..i.e DT
+            fetchListDTUrl:function () {
+                return   API_HOST+'/companies/DT'
+            },    
+
             fetchSingleByID: function (id) {
-                
+               return $http.get(API_HOST+'/companies/'+id) 
             }
 
 
