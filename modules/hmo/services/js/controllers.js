9e8afea6df4e055dd5630c06ab865225d597d6e4
Habeeb Mac-iver <habeeb.maciver@aiesec.net>
2018-09-04 04:25:09 +0100

    Service module crud completed

--- modules/hmo/services/js/controllers.js
+++ modules/hmo/services/js/controllers.js
@@ -3,109 +3,95 @@
  */
 //enrollees List controller
 angular.module('BmsApp')
-    .controller('HmoServiceListCtrl', function($scope,$compile,$activityIndicator,EnrolleeService,$state,DTColumnBuilder,DTOptionsBuilder,UserService) {
+    .controller('HmoServiceListCtrl', function ($scope,$compile,$activityIndicator,ServicesService,$state,DTColumnBuilder,DTOptionsBuilder,UserService) {
 
-    $scope.dtInstance = {}; //instance ref for data tables
-    $scope.filters = {}; // filters
+        $scope.dtInstance = {}; //instance ref for data tables
+        $scope.filters = {}; // filters
 
 
 
-    //init options for datatable grid on this scope
-    $scope.dtOptions = DTOptionsBuilder.newOptions()
-        .withOption('ajax', {
-            // Either you specify the AjaxDataProp here
-            // dataSrc: 'data',
-            url: EnrolleeService.fetchListDTUrl(),
-            type: 'GET',
-            data: $scope.filters,
-            headers: {
-                Authorization: 'Bearer ' + UserService.loadToken()
+        //init options for datatable grid on this scope
+        $scope.dtOptions = DTOptionsBuilder.newOptions()
+            .withOption('ajax', {
+                // Either you specify the AjaxDataProp here
+                // dataSrc: 'data',
+                url: ServicesService.fetchListDTUrl(),
+                type: 'GET',
+                data: $scope.filters,
+                headers: {
+                    Authorization: 'Bearer ' + UserService.loadToken()
+                }
+            })
+            // or here
+            .withDataProp('data')
+            .withOption('processing', true)
+            .withOption('serverSide', true)
+            .withPaginationType('full_numbers')
+
+            .withOption('fnRowCallback',
+                function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+                    $compile(nRow)($scope);
+                });
+
+        //create columns for this grid
+        $scope.dtColumns = [
+            DTColumnBuilder.newColumn('name').withTitle('Name').notSortable(),
+            DTColumnBuilder.newColumn('type_code').withTitle('Type'),
+            DTColumnBuilder.newColumn('created_at').withTitle('Created'),
+            DTColumnBuilder.newColumn('action').withTitle('').notSortable()
+                .renderWith(function(data, type, full) {
+                    var actions = [];
+                    var view = '<a ng-click="deleteService(' + full.id + ')" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i></a>';
+                    actions.push(view);
+                
+                    return actions.join(" ");
+                })
+            ,
+        ];
+        
+        $scope.deleteService = function(id) {
+            var r = confirm("Are you sure you want to delete this service?")
+            if (r == true) {
+                $activityIndicator.startAnimating();
+                ServicesService.deleteService(id)
+                    .success(function (resp) {
+                        $activityIndicator.stopAnimating()
+                        swal('Alert!', 'Service deleted', 'info')
+                        $state.reload();
+                    })
+                    .error(function (err) {
+                        $activityIndicator.stopAnimating()
+                        swal('Error!', err.message, 'error')
+                    })
             }
-        })
-        // or here
-        .withDataProp('data')
-        .withOption('processing', true)
-        .withOption('serverSide', true)
-        .withPaginationType('full_numbers')
+        }
+    })
 
-        .withOption('fnRowCallback',
-            function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
-                $compile(nRow)($scope);
-            });
-
-    //create columns for this grid
-    $scope.dtColumns = [
-        DTColumnBuilder.newColumn('insurance_no').withTitle('Insurance No').notSortable(),
-        DTColumnBuilder.newColumn('first_name').withTitle('Name')
-            .renderWith(function (data,type,full) {
-               return data +' '+full.last_name
-            }),
-        
-        DTColumnBuilder.newColumn('phone').withTitle('Phone'),
-        DTColumnBuilder.newColumn('sex').withTitle('Sex')
-            .renderWith(function (data,type,full) {
-               return EnrolleeService.getSex(data)
-            }),
-        DTColumnBuilder.newColumn('enrollee_plan_id').withTitle('Plan')
-            .renderWith(function (data,type,full) {
-                if(full.plan)
-                    return full.plan.name
-                else  return ''
-            }),
-        
-        DTColumnBuilder.newColumn('enrollee_status_code').withTitle('Status').notSortable()
-            .renderWith(function(data,type,full) {
-                if(full.status)
-                    return full.status.name
-                else  return ''
-            }),
-        DTColumnBuilder.newColumn('created_at').withTitle('Created'),
-        DTColumnBuilder.newColumn('action').withTitle('').notSortable()
-            .renderWith(function(data, type, full) {
-                var actions = [];
-                var view = '<a  class="btn btn-default btn-xs"><i class="fa fa-eye"></i></a>';
-                actions.push(view);
-               
-                return actions.join(" ");
-            })
-        ,
-    ];
-})
-
-//enrollee create / edit controller
+    //enrollee create / edit controller
     .controller('HmoServiceCreateCtrl', function ($scope,$state,$activityIndicator,ServicesService) {
-    // enrollee companies
-    // $scope.companies = [];
-    // $scope.searchCompanies = function (q) {
-    //     CompaniesService.fetchList(q,10).success(function (resp) {
-    //         $scope.companies = resp;
-    //     })
-    // }
-
-    $scope.types = []
-    ServicesService.fetchServiceTypes()
-        .success(function (resp) {
-            $scope.types = resp
-        })
-        .error(function(err) {
-            console.log(err)
-        })
-
-    //save service
-    $scope.saveService = function () {
-        $activityIndicator.startAnimating();
-        console.log($scope.service)
-        ServicesService.createService($scope.service)
+        $scope.types = []
+        ServicesService.fetchServiceTypes()
             .success(function (resp) {
-                $activityIndicator.stopAnimating()
-                $scope.service = {}
-                swal('Success', 'Service Created Successfully', 'success')
+                $scope.types = resp
             })
-            .error(function (err) {
-                $activityIndicator.stopAnimating()
-                swal('Error!', err.message, 'error')
+            .error(function(err) {
+                console.log(err)
             })
-    }
-    
-});
+
+        //save service
+        $scope.saveService = function () {
+            $activityIndicator.startAnimating();
+            ServicesService.createService({ service: $scope.service })
+                .success(function (resp) {
+                    $activityIndicator.stopAnimating()
+                    $scope.service = {}
+                    swal('Success', 'Service Created Successfully', 'success')
+                })
+                .error(function (err) {
+                    $activityIndicator.stopAnimating()
+                    swal('Error!', err.message, 'error')
+                })
+        }
+        
+    })
 
