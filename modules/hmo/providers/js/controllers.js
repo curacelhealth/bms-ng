30872bd02e94f0149e858b7cdc84efa28be0bbbf
Habeeb Mac-iver <habeeb.maciver@aiesec.net>
2018-09-07 16:26:17 +0100

    Still an issue with spreadsheet file uploading

--- modules/hmo/providers/js/controllers.js
+++ modules/hmo/providers/js/controllers.js
@@ -360,17 +360,34 @@ angular.module('BmsApp')
     }
 
     $scope.uploadFile = function () {
-        $scope.btn_disable = true
-        var file = $scope.myFile;
-        ProviderService.uploadProvidersByExcel(file)
-            .success(function (response) {
-                $scope.btn_disable = false
-                $('#importModal').modal('hide');
-                // $state.reload();
-                swal('Success', "Providers successfully imported", 'success');
-            })
-            .error(function (response) {
-                swal('Error!', response.message, 'error');
-            });
+        if($scope.ignore == true) {
+            var data = { "ignore_top_row": 1, "file": $scope.myFile }
+            console.log(data)
+            // $scope.btn_disable = true
+            // ProviderService.uploadProvidersByExcel($scope.myFile)
+            //     .success(function (response) {
+            //         $scope.btn_disable = false
+            //         var element = angular.element("#importModal");
+            //         element.modal('hide')
+            //         $state.reload();
+            //         swal('Success', "Providers successfully imported", 'success');
+            //     })
+            //     .error(function (response) {
+            //         swal('Error!', response.message, 'error');
+            //     });
+        } else if ($scope.ignore == undefined) {
+            var data = { "ignore_top_row": 0, "file": $scope.myFile }
+            $scope.btn_disable = true
+            ProviderService.uploadProvidersByExcel(data)
+                .success(function (response) {
+                    $scope.btn_disable = false
+                    var element = angular.element("#importModal");
+                    element.modal('hide')
+                    swal('Success', "Providers successfully imported", 'success');
+                })
+                .error(function (response) {
+                    swal('Error!', response.message, 'error');
+                });
+        }
     }
 });
